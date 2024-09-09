60633346437093941eeeab63af168b6c04e07927
Denis Kobzar <laycode00700@gmail.com>
2024-09-09 11:52:22 +0300

    fix +

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,9 @@
 from flask import Flask 
-#from .routes import main
-import db
+from .routes import main
 
 def create_app():
     app = Flask('api')
 
-    #app.register_blueprint(main)
+    app.register_blueprint(main)
 
     return app
-
-db.add_record('item 1')
-db.add_record('item 2')
-db.add_record('item 3')
-
-print(db.get_records())
