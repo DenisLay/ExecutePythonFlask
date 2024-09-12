03b58e58ea38ca605d1fb7702f3088a5808dc852
Denis Kobzar <laycode00700@gmail.com>
2024-09-12 10:03:00 +0300

    fix last commit

--- app/script_builder.py
+++ app/script_builder.py
@@ -23,16 +23,16 @@ def execute_code(src):
 
     json_items = []
     for key, value in local_vars.items():
-        #if isinstance(value, (types.ModuleType, types.FunctionType, types.BuiltinFunctionType)):
-        #    json_value = str(type(value))
-        #else:
-        #    try:
-        #        json_value = json.dumps(value)
-        #    except (TypeError, OverflowError):
-        #        json_value = str(value)
+        if isinstance(value, (types.ModuleType, types.FunctionType, types.BuiltinFunctionType)):
+            json_value = str(type(value))
+        else:
+            try:
+                json_value = json.dumps(value)
+            except (TypeError, OverflowError):
+                json_value = str(value)
 
         if key == 'result':
-            json_items.append({'key': key, 'value': value})
+            json_items.append({'key': key, 'value': json_value})
         #json_items.append({'key': key, 'value': json_value})
 
     result = json.dumps(json_items, indent=4)
