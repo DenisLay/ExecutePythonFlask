ba85d4d8576f3fad69c0f83820ef268bea04b3ef
Denis Kobzar <laycode00700@gmail.com>
2024-09-16 12:44:05 +0300

    remove import_package

--- app/script_builder.py
+++ app/script_builder.py
@@ -11,44 +11,11 @@ def execute_code(src):
     new_stdout = io.StringIO()
     sys.stdout = new_stdout
 
-    pre_code = """
-import importlib
-import subprocess
-import sys
-
-logs = []
-
-def import_package(name, package):
-    logs = []
-
-    try:
-        logs.append('try to import module.')
-        importlib.import_module(name)
-        logs.append('module imported.')
-    except ImportError as e:
-        try:
-            logs.append('ImportError.')
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
-            logs.append('package installed.')
-            importlib.import_module(name)
-            logs.append('try to import.')
-        except subprocess.CalledProcessError as e:
-            logs.append('subprocess.CalledProcessError.')
-            result = f'{str(e)} || exit 1'
-            sys.exit(1)
-        except ImportError  as e:
-            logs.append('ModuleNotFoundError.')
-            result = f'{str(e)} || exit 2'
-            sys.exit(2)
-    """
-
-    src = f'{pre_code}\n\n\n{src}'
-
     try:
         exec(src, globals(), local_vars)
     except Exception as e:
         error = str(e)
-        return json.dumps({'error-first': f'{error} (RUNTIME)'})
+        return json.dumps({'runtime error': f'{error}'})
     finally:
         sys.stdout = old_stdout
 
@@ -64,11 +31,8 @@ def import_package(name, package):
             except (TypeError, OverflowError):
                 json_value = str(value)
 
-        #if key == 'result' or key == 'logs':
-        json_items.append({'key': key, 'value': json_value})
-        #json_items.append({'key': key, 'value': json_value})
-
-    json_items.append({'key': 'src', 'value': src})
+        if key == 'result':
+            json_items.append({'key': key, 'value': json_value})
 
     result = json.dumps(json_items, indent=4)
 
