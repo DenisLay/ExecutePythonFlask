5760ace94599beec6fbc932b8470e4b05f17cbc3
Denis Kobzar <laycode00700@gmail.com>
2024-09-16 16:28:01 +0300

    fix

--- app/db.py
+++ app/db.py
@@ -29,7 +29,7 @@ class DBBot:
             columns_script = ''
 
             for column in columns:
-                column_script = f'{column['name']},'
+                column_script = f'{column.get('name')},'
                 columns_script += column_script
 
             script = f'CREATE TABLE {table_name} ({columns_script});'
